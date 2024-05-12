399ccd0ef3d31ad18e9ff33039d33d2066069453
Kali07 <richardmulamba16@gmail.com>
2024-05-12 20:47:12 +0200

    creation fichiers heaser + footer + css

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/header-footer.css" rel="stylesheet">
+</head>
+<body>
+    <footer>
+        <p>© 2024 Gestion de Stock. Tous droits réservés.</p>
+        <p>Contactez-nous : contact@juniorwawite.com</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/header-footer.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="main-nav">
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="creation_Compte.php">Inscription</a></li>
+                <li><a href="#"> Mon compte</a></li> 
+                <!-- <img src="" alt="Compte" style="vertical-align: middle;"> -->
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
