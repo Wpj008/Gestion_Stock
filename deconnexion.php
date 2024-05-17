ecd15cb896187a1639cb1fb9d32532eca6d120c2
WPJ008 <wawitephilemon@gmail.com>
2024-05-17 03:33:06 +0200

    creation page deconnexion

---
+++ deconnexion.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+session_destroy();
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="#" />
+  <title>DECONNEXION</title>
+</head>
+
+<body>
+
+    <p> Voulez vous vous deconnecter ? </p>
+<form action="index.php" method="POST">
+
+
+                <br><br><br><br><br><br>
+
+
+                <button id="valider">DECONNEXION</button>
+
+</form>
+
+                </body>
+
+                
+</html>
