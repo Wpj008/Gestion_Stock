5e9f9e9caa79a7548f4b274e13fe35b999be0eb1
WPJ008 <wawitephilemon@gmail.com>
2025-11-23 17:48:18 +0100

    function update

--- functions/saleFunction.php
+++ functions/saleFunction.php
@@ -2,28 +2,29 @@
 include "data.php";
 
 
-//function pour afficher all products + requete de jointure entre product, category  and supplier
-function selctAllSale(){
+//function pour afficher all products 
+function selectAllSale(){
 
     try{
 
-     $query = $GLOBALS['data']->prepare("SELECT * FROM products");
+         $query = $GLOBALS['data']->prepare("SELECT * FROM products");
 
 
-$query->execute();
+         $query->execute();
   
-$produits = $query->fetchAll();
+         $produits = $query->fetchAll();
 
-if($produits){
+         if($produits){
 
-    return $produits;
-}
+         return $produits;
+        }
 
 
-} catch(PDOException $e){
+    }
+    catch(PDOException $e){
 
-    return "Il y a eu un probleme de la recuperation des infos des produits" . $e->getMessage();
-}
+        return "Il y a eu un probleme de la recuperation des infos des produits" . $e->getMessage();
+     }
 
 
 }
@@ -50,27 +51,118 @@ function registerSale($user_id, $status, $total_sale) {
 function registerRetailSale($sale_id, $product_ids, $quantities, $prices) {
 
     try {
+
         foreach ($product_ids as $index => $product_id) {
 
-            $qty = $quantities[$index];
+            $quatityCommand = $quantities[$index];
             $price = $prices[$index];
-            $total = $qty * $price;
+            $total = $quatityCommand * $price;
 
-            $query = $GLOBALS['data']->prepare("INSERT INTO retail_sales(sale_id, product_id, quantity_retailSale, unit_price_retailSale, grand_total_retailSale) VALUES (:sale_id, :product_id, :qty, :price, :total)");
+            // Vérifie que nombre valide
+            if (is_numeric($quatityCommand) && $quatityCommand > 0) {
 
-            $query->bindParam(':sale_id', $sale_id);
-            $query->bindParam(':product_id', $product_id);
-            $query->bindParam(':qty', $qty);
-            $query->bindParam(':price', $price);
-            $query->bindParam(':total', $total);
+                // Récupération du stock actuel du produit
+                $queryStock = $GLOBALS['data']->prepare("SELECT quantity_product FROM products WHERE id_product = :id");
+                $queryStock->bindParam(':id', $product_id);
+                $queryStock->execute();
+                $Stock = $queryStock->fetchColumn();
 
-            $query->execute();
+
+                //Calcul du nouveau stock
+                $newQuantite = $Stock - $quatityCommand;
+
+
+                //Vérification du stock
+                if ($newQuantite >= 0) {
+
+                    // Mise à jour du stock dans la BDD
+                    $queryUpdate = $GLOBALS['data']->prepare("UPDATE products SET quantity_product = :quantite WHERE id_product = :id");
+
+                    $queryUpdate->bindParam(':quantite', $newQuantite);
+                    $queryUpdate->bindParam(':id', $product_id);
+                    $queryUpdate->execute();
+
+
+                    // Insertion de la ligne dans retail_sales
+                    $queryInsert = $GLOBALS['data']->prepare("INSERT INTO retail_sales(sale_id, product_id, quantity_retailSale, unit_price_retailSale, grand_total_retailSale)VALUES (:sale_id, :product_id, :qty, :price, :total)");
+
+                    $queryInsert->bindParam(':sale_id', $sale_id);
+                    $queryInsert->bindParam(':product_id', $product_id);
+                    $queryInsert->bindParam(':qty', $quatityCommand);
+                    $queryInsert->bindParam(':price', $price);
+                    $queryInsert->bindParam(':total', $total);
+
+                    $queryInsert->execute();
+
+                    if($newQuantite >= 5 && $newQuantite <= 10){
+
+                        $etat = 6;
+
+                        // Mise à jour du status du produit dans la BDD
+                        $queryUpdate = $GLOBALS['data']->prepare("UPDATE products SET product_status = :etat WHERE id_product = :id");
+    
+                        $queryUpdate->bindParam(':etat', $etat);
+                        $queryUpdate->bindParam(':id', $product_id);
+                        $queryUpdate->execute();
+
+
+                    }
+
+                    if($newQuantite >= 0 && $newQuantite < 5){
+
+                        $etat = 7;
+
+                        // Mise à jour du status du produit dans la BDD
+                        $queryUpdate = $GLOBALS['data']->prepare("UPDATE products SET product_status = :etat WHERE id_product = :id");
+    
+                        $queryUpdate->bindParam(':etat', $etat);
+                        $queryUpdate->bindParam(':id', $product_id);
+                        $queryUpdate->execute();
+                    }
+
+                } else {
+
+                    echo "<p style='color:red;'>Quantité insuffisante pour le produit commandé</p>";
+                }
+
+            } else {
+                echo "<p style='color:red;'>La quantité saisie n'est pas un nombre valide.</p>";
+            }
         }
 
-    }
-    catch (PDOException $e) {
+    } catch (PDOException $e) {
+
         return "Erreur produits : " . $e->getMessage();
     }
+}
+
+
+//function de jointure entre tables sales, retail_sale, product
+
+function InnerJoinTableSale(){
+
+    try{
+
+        $query = $GLOBALS['data']->prepare("SELECT * FROM retail_sales INNER JOIN products ON retail_sales.product_id = products.id_product INNER JOIN sales ON retail_sales.sale_id = sales.id_sale INNER JOIN users ON sales.user_id = users.id_user");
+
+        $query->execute();
+
+        $sales = $query->fetchAll();
+
+        if($sales){
+
+        return $sales;
+        }
+
+
+
+
+    }
+    catch(PDOException $e){
+    
+         return "Erreur lors de la recuperation des informations". $e->getMessage();
+
+        }
 }
 
 ?>
