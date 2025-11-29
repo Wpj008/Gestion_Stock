fefe47d64b7283277a4ec6012996aad047cfd5c9
WPJ008 <wawitephilemon@gmail.com>
2025-11-29 21:09:28 +0100

    finition des petits details

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,6 +3,9 @@ session_start();
 include "../data.php";
 include "../functions/purchaseFunction.php";
 include "../functions/productFunction.php";
+include "../functions/userFunction.php";
+include "../functions/supplierFunction.php";
+include "../functions/saleFunction.php";
 
 
 // Vérifier si l'utilisateur est connecté
@@ -13,22 +16,50 @@ if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
 
 //on recupere les infos du purchase 
 
-$callPurchase = InnerJoinAllPurchase();
-$callProduct = selectAllProduct();
+$callPurchase = InnerJoinAllPurchase();//affiche la requete de jointure sur purchase et retailpurchase
+$callProduct = selectAllProduct();//affiche tous les produits
+$AllPurchase = selectAllPurchase();//affiche tous les achats
+$numberUser = countUsers();//compte le nombre d'utilisateur
+$numberSupplier = countSuppliers();//compte le nombre de fournisseur
+$numberSale = countSales();//compte le nombre de sale
+$numberPurchase = countPurchases();//compte le nombre de purchase
 
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-valider'])){
 
+    //recuperations des id de purchase et retailpurchase
+    $idretailPurchase = $_POST['ID_retailpurchase'];
     $idPurchase = $_POST['ID_purchase'];
 
+    //new status
     $futurStatus = 2;
-    
-    
-    $queryUpdate = $GLOBALS['data']->prepare("UPDATE purchases SET status_id = :status_id WHERE id_purchase = :id_purchase");
 
-    $queryUpdate->bindParam(':id_purchase', $idPurchase);
+    //Requete pour modifier le status de l'article dans retailpurchase
+    $queryUpdate = $GLOBALS['data']->prepare("UPDATE retail_purchases SET status_id_retail = :status_id WHERE id_retailPurchase = :id_retailpurchase");
+
+    $queryUpdate->bindParam(':id_retailpurchase', $idretailPurchase);
     $queryUpdate->bindParam(':status_id', $futurStatus);
 
     $queryUpdate->execute();
+
+    //requete qui compte le nombre d'article qui ont l'id status != 2
+    $querySelect = $GLOBALS['data']->prepare("SELECT COUNT(*) FROM retail_purchases WHERE purchase_id = :purchase_id AND status_id_retail != 2");
+    $querySelect->bindParam(':purchase_id', $idPurchase);
+
+
+    $querySelect->execute();
+
+    $result = $querySelect->fetchColumn();//ca affiche uniquement la colonne purchase id dans retailpurchase
+
+    //si count(*) == 0 ; (cas d'une commande avec plusieurs articles)toutes les articles dans retailpurchase ont status_id == 2 alors on update le status_id dans purchase
+    if($result == 0){
+    $queryUpdateSelect = $GLOBALS['data']->prepare("UPDATE purchases SET status_id = :status_id WHERE id_purchase = :id_purchase");
+
+    $queryUpdateSelect->bindParam(':id_purchase', $idPurchase);
+    $queryUpdateSelect->bindParam(':status_id', $futurStatus);
+
+    $queryUpdateSelect->execute();
+
+}
 
 }
 
@@ -99,23 +130,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
         <!-- Stats -->
         <section class="stats-grid">
             <div class="stat-card red">
-                <h3>Total Clients</h3>
-                <p class="value">1</p>
+                <h3>Total Employés</h3>
+                <p class="value"><?= $numberUser['total'] ?></p>
             </div>
 
             <div class="stat-card green">
                 <h3>Total Fournisseurs</h3>
-                <p class="value">1</p>
+                <p class="value"><?= $numberSupplier['valeur'] ?></p>
             </div>
 
             <div class="stat-card blue">
                 <h3>Total Ventes</h3>
-                <p class="value">19 000.00</p>
+                <p class="value"><?= $numberSale['total'] ?></p>
             </div>
 
             <div class="stat-card gray">
                 <h3>Total Achats</h3>
-                <p class="value">191 845.00</p>
+                <p class="value"><?= $numberPurchase['total'] ?></p>
             </div>
         </section>
 
@@ -159,7 +190,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
 
                   
 
-                    if($purchase['status_id'] == 1){
+                    if($purchase['status_id_retail'] == 1){
 
                         $i++;
                     
@@ -175,6 +206,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
 
                      <td>
                         <form method="POST">
+                            <input name="ID_retailpurchase" type="hidden" value="<?= $purchase['id_retailPurchase'] ?>"/>
                             <input name="ID_purchase" type="hidden" value="<?= $purchase['id_purchase'] ?>"/>
                         <input name="submit-valider" type="submit" class="btn-view" value="Valider"/>
                         <input name="submit-delete" type="submit" class="btn-delete" value="Annuler"/>
@@ -216,7 +248,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
                             <td><?= $purchase['name_product'] ?></td>
                             <td><?= $purchase['quantity_retailPurchase'] ?></td>
                             <td><?= $purchase['name_supplier'] ?></td>
-                            <td><?= $purchase['grand_total_retailPurchase'] ?></td>
+                            <td><?= $purchase['total_purchase'] ?></td>
 
                      <td>
                         <form method="POST">
@@ -292,6 +324,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
         <?php
         $i = 0;
         foreach($callProduct as $product){
+
+            if($product['quantity_product'] < 10){
             $i ++;
             ?>
             <tr>
@@ -307,7 +341,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit-confirmer'])){
 
             </tr>
 
-        <?php }?>
+        <?php } }?>
         </tbody>
     </table>
 
