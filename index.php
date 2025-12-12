de318e53a73b4517f20fa6e8729cbd3fc5ae0327
WPJ008 <wawitephilemon@gmail.com>
2025-12-12 23:05:00 +0100

    improvement security

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"  && isset($_POST['submit'])) {
 
 $email = $_POST['username'];
 
-$password = htmlspecialchars($_POST['password']);
+$password = htmlspecialchars($_POST['password_3']);
 
 
    
@@ -20,8 +20,10 @@ $password = htmlspecialchars($_POST['password']);
         if ($login) {
 
             $role = $_SESSION['role'];
+          
+            $lengthPasasword = strlen($password);            
 
-            if ($password == '0000'){
+           if ($password == '12345678'){
 
                 $query = $GLOBALS['data']->prepare("SELECT * FROM users WHERE email_user = :email");
                     $query->bindParam(':email', $email);
@@ -34,12 +36,19 @@ $password = htmlspecialchars($_POST['password']);
                         exit();
             
             
-             } else if ($role == "gestionnaire"){
+             } 
+             
+             if( $lengthPasasword < 8 ){
+
+                $errormessage = "Taille de mot de passe insuffisante.";
+              
+   
+               } else if ($role == "gestionnaire"){
                
             header('Location: admin/login.php');
             exit;
 
-        } else{
+        }else{
 
             header('Location: home.php');
         }
@@ -48,13 +57,9 @@ $password = htmlspecialchars($_POST['password']);
         $errormessage = "Email ou mot de passe incorrect.";
         }
 
- 
-
 
 }
 ?>
-
-
 
 
 
@@ -66,19 +71,21 @@ $password = htmlspecialchars($_POST['password']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - Gestion de Stock</title>
     <link rel="stylesheet" href="css/index.css">
+    <script src="js/verification.js"></script>
 </head>
 <body>
     <div class="login-container">
         <h1>Gestion de Stock</h1>
         <h2>Connexion</h2>
-        <form action="" method="POST">
+        <form action="" method="POST" onsubmit = "return verificationPassword();">
             <div class="input-group">
                 <label for="username">Email:</label>
                 <input type="email" id="username" name="username" required>
             </div>
             <div class="input-group">
                 <label for="password">Mot de passe:</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password_3" name="password_3" required>
+                <div id="passwordEror" class="error-message" style="color : red;"></div>
             </div>
 
            
@@ -90,5 +97,6 @@ $password = htmlspecialchars($_POST['password']);
     </div>
 
     <?php include "footer.php";?>
+    
 </body>
 </html>
